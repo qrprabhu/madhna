abaf1925df4ff93dc221aa50ed982d75270d0dce
qrprabhu <qrprabhu@gmail.com>
2026-05-19 23:48:50 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart({
       server: { entry: "./src/server.ts" },
+    }),
+    nitro({
+      preset: process.env.VERCEL ? "vercel" : undefined,
     }),
     viteReact(),
   ],
